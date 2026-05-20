8738cf10415b386fe8c9e3316242df6b3502b952
Dmytro Podoprosvietov <dp.quant@gmail.com>
2026-05-20 16:41:43 +0300

    fix: update celery signal to worker_process_init to ensure proper OpenTelemetry instrumentation post-fork

--- st_common_data/opentelemetry/celery/setup.py
+++ st_common_data/opentelemetry/celery/setup.py
@@ -10,4 +10,7 @@ def setup_telemetry(app: Celery, init_function: Callable[[None], None]) -> None:
     app.conf.update(
         worker_hijack_root_logger=False,
     )
-    signals.celeryd_init.connect(lambda **_: init_function(), weak=False)
+    # worker_process_init is crucial: it runs AFTER the process forks.
+    # OpenTelemetry uses background threads for exporting metrics. If initialized before fork,
+    # those threads don't survive in the child processes and no metrics will be sent.
+    signals.worker_process_init.connect(lambda **_: init_function(), weak=False)
